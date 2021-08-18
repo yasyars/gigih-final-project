0bf4f75e5e48cfe31b6a4af713328831c4c3f907
yasyars <yasya.rusyda@gmail.com>
2021-08-18 14:43:13 +0700

    Add tested comment inherit to post

--- backend/models/comment.rb
+++ backend/models/comment.rb
@@ -1,22 +1,11 @@
 require_relative '../db/db_connector'
-require_relative 'user'
+require_relative 'post'
 
-class Comment 
-  attr_reader :id, :content, :user, :attachment, :timestamp, :hashtags
+class Comment < Post
 
   def initialize(param)
-    @id = param[:id] ? param[:id] : nil
-    @content = param[:content]
+    super(param)
     @user = param[:user]
-    @post = param[:post]
-    @attachment = param[:attachment] ? param[:attachment] : nil
-    @timestamp = param[:timestamp] ? param[:timestamp] : nil
-    @hashtags = param[:hashtags] ? param[:hashtags] : []
   end
-
-  def valid?
-    @content.length <= 1000 && @content.gsub(/\s+/, "")!=""
-  end
-
 
 end
--- backend/spec/models/comment_spec.rb
+++ backend/spec/models/comment_spec.rb
@@ -46,7 +46,7 @@ describe Comment do
         user = double
         post = double
 
-        comment = Comment.new({
+       comment = Comment.new({
           content: "Lorem ipsum dolor sit amet, consectetur adipiscLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ante magna, gravida at porta sed, interdum quis tellus. Proin finibus iaculis ipsum, nec eleifend nibh condimentum ac. Aliquam luctus rhoncus facilisis. Phasellus molestie odio feugiat odio aliquam facilisis. Aliquam convallis arcu sed pulvinar rhoncus. Vivamus a suscipit sem. Nunc vel mi sed orci lacinia aliquet et vel mi. Nulla vulputate rhoncus metus, vitae venenatis est. Nunc enim nunc, congue sed arcu id, sollicitudin luctus purus. Curabitur a nibh eget risus convallis pulvinar. Nunc dignissim ex sed massa pharetra, sed pellentesque tellus auctor. Phasellus dictum eget turpis ut consectetur. Ut risus lectus, dignissim eget ex quis, lobortis sagittis nibh. Duis tempus purus lorem. Morbi nisi lorem, volutpat nec hendrerit ut, suscipit at tortor. Maecenas at sem placerat, sollicitudin lacus eget, volutpat nunc. Donec sagittis magna eget ante sagittis, eget ornare nisi iaculis.",
           user: user,
           post: post
@@ -68,6 +68,68 @@ describe Comment do
         })
 
         expect(comment.valid?).to be false
+      end
+    end
+  end
+
+  describe '#extract_hashtag' do
+    context 'when initialized with no hashtag' do
+      it 'should return empty array' do
+        user = double
+        post =double
+
+        comment = Comment.new({
+          content: "Hai semua, selamat pagi semoga harimu menyenangkan",
+          user: user,
+          post: post
+        })
+
+        expect(comment.extract_hashtag).to eq([])
+      end
+    end
+
+    context 'when initialized with a hashtag' do
+      it 'should return array with a member' do
+        user = double
+        post =double
+
+        comment = Comment.new({
+          content: "Hai semuanya, bagus gak pakaianku? #ootd",
+          user: user,
+          post: post
+        })
+
+        expect(comment.extract_hashtag).to eq(['#ootd'])
+      end
+    end
+
+    context 'when initialized with 2 same hashtag' do
+      it 'should return array with one member' do
+        user = double
+        post = double
+
+        comment = Comment.new({
+          content: "Hai semuanya, bagus gak pakaianku? #ootd #oOtD",
+          user: user,
+          post: post
+        })
+
+        expect(comment.extract_hashtag).to eq(['#ootd'])
+      end
+    end
+
+    context 'when initialized with 2 different hashtag' do
+      it 'should return array with 2 members' do
+        user = double
+        post = double
+
+        comment = Comment.new({
+          content: "Hai semuanya, bagus gak pakaianku? #ootd #sunday",
+          user: user,
+          post: post
+        })
+
+        expect(comment.extract_hashtag).to eq(['#ootd','#sunday'])
       end
     end
   end
