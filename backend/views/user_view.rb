4ea2a4156530bc46bcc39b3b7435b1e739bcde8c
yasyars <yasya.rusyda@gmail.com>
2021-08-20 12:48:24 +0700

    Refactor message to use constant

--- backend/views/user_view.rb
+++ backend/views/user_view.rb
@@ -1,8 +1,24 @@
 require_relative '../models/user'
 require 'json'
 
+MESSAGE = {
+  :create_success => 'User successfully created',
+  :get_success =>'User data retrieved succesfully',
+  :get_not_found => 'User data not found'
+}
+
 class UserView
   def create_success
-    {'message' => 'User successfully created'}.to_json
+    {'message' => MESSAGE[:create_success]}.to_json
+  end
+
+  def user_data(user)
+    {'message' => MESSAGE[:get_success],
+      'data' => user.to_hash}.to_json
+  end
+
+  def empty_user
+    {'message' => MESSAGE[:get_not_found],
+      'data' => nil}.to_json
   end
 end
