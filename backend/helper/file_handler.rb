20f85772bbe1cacd4598830e4111ccd4b31ef1da
yasyars <yasya.rusyda@gmail.com>
2021-08-24 00:45:28 +0700

    Add raise error in file handler if attachment type is invalid

--- backend/helper/file_handler.rb
+++ backend/helper/file_handler.rb
@@ -4,19 +4,21 @@ class FileHandler
   PATH = 'uploads/'
 
   def upload(file)
-    path_file = generate_new_name_if_exist(file)
+    begin
+      path_file = generate_new_name_if_exist(file)
+    rescue
+      raise TypeError,"Attachment type is not valid, make sure it is a png, jpg, txt, or other types of file"
+    end
+
     File.open(path_file, 'w') do |f|
       f.write(file[:tempfile].read)
     end
     path_file
   end
 
-  def generate_new_name_if_exist(file)
-    begin
-      file_name = file[:filename].gsub(/\s+/, '')
-    rescue
-      raise TypeError,"Attachment type is not valid, make sure it is a png, jpg, txt, or other types of file"
-    end
+  def generate_new_name_if_exist(file)   
+    file_name = file[:filename].gsub(/\s+/, '')
+ 
     path_file = PATH + file_name
     i = 1
     while exist?(path_file)
